8e05d7ca7f2e463c0e7c9888dcb49c168d22f156
sritapam <pereyrapame@gmail.com>
2021-09-25 04:50:09 -0300

    Model Activity

---
+++ PI-Countries-Henry/api/src/models/Activity.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) =>{
+    sequelize.define('Activity', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique:true,
+        },
+        difficulty: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        season: {
+            type: DataTypes.ENUM('Spring', 'Summer','Autumn', 'Winter'),
+            allowNull: true,
+        },
+
+    })
+}
