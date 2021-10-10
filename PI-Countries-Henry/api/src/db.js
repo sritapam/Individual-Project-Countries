a8bd576e7c6289554a88adad279f79eb8f992e9c
sritapam <85538485+sritapam@users.noreply.github.com>
2021-10-10 13:34:26 -0300

    Add files via upload

--- PI-Countries-Henry/api/src/db.js
+++ PI-Countries-Henry/api/src/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config();//.env para llamar a las variables de entorno
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
@@ -8,8 +8,7 @@ const {
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
   logging: console.log('Sequelize Ready'), // set to console.log to see the raw SQL queries
-  native: false,
-   // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
 
@@ -35,9 +34,14 @@ const { Country, Activity } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Country.belongsToMany(Activity, { through: 'CountryActivity' });
-Activity.belongsToMany(Country, { through: 'CountryActivity' });
-
+Country.belongsToMany(Activity, {
+  through: 'activities_country',
+  timestamps: false,
+});
+Activity.belongsToMany(Country, {
+  through: 'activities_country',
+  timestamps: false,
+});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
---
+++ PI-Countries-Henry/api/src/utils/index.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+const { Country, Activity } = require("../db.js");
+
+
+const getFromApi = async () => {
+  
+  try{ 
+  const { data } = await axios.get("https://restcountries.com/v3/all");
+  
+  const dataFromApi = data.map((c) => {
+    return {
+      name: c.name.common,
+      alpha3Code: c.cca3,
+      capital: c.capital ? c.capital[0] : "capital not found",
+      continent: c.region,
+      area: c.area,
+      region: c.subregion,
+      flags: c.flags.find((el)=>el.includes('svg')),
+    };
+  });
+
+await Country.destroy({
+  where: {
+
+  }
+})
+
+ const dB = await Country.bulkCreate(dataFromApi);
+
+  
+}
+ catch (e) {
+  (e) => console.log(e);
+  }
+  }
+
+const getFromDb = async () => {
+    const countriesinDb = await Country.findAll(
+      {
+        include: {
+          model: Activity,
+          attributes: ['name', 'difficulty', 'duration', 'season'],
+          through: { attributes: [] },
+        },
+      }
+    );
+    return countriesinDb;
+  };
+
+
+module.exports = {
+    getFromApi,
+    getFromDb,
+};
