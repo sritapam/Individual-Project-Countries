675801c0525d8695476ae53b16254797ba93127e
sritapam <85538485+sritapam@users.noreply.github.com>
2021-10-10 13:33:43 -0300

    Add files via upload

---
+++ PI-Countries-Henry/api/src/routes/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const router = Router();
+
+const { getAllCountries, getCountryById } = require('../controllers/country');
+const { postActivity, getActivity } = require('../controllers/activity')
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.get('/countries', getAllCountries);
+router.get('/countries/:idPais', getCountryById);
+router.post('/activity', postActivity);
+router.get('/activity', getActivity);
+
+
+module.exports = router;
