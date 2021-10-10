e0a7b2316e91a5ed72c81c0781fde231b6a33cd1
sritapam <85538485+sritapam@users.noreply.github.com>
2021-10-10 12:04:53 -0300

    Add files via upload

---
+++ PI-Countries-Henry/api/src/controllers/activity.js
@@ -0,0 +1,39 @@
+const { Country, Activity } = require("../db.js");
+const { Op } = require("sequelize");
+
+const postActivity = async (req, res) => {
+  try {
+    const { name, difficulty, duration, season, createdInDb, countries } =
+      req.body;
+
+    const [activityCreated] = await Activity.findOrCreate({
+      where: { [Op.and]: [{ name: name }] },
+      defaults: { name, difficulty, duration, season, createdInDb },
+    });
+    var auxiliar = await Promise.all(
+      countries.map((c) => {
+        var country = Country.findOne({
+          where: { name: c },
+        });
+        return country;
+      })
+    );
+    var auxiliar2 = await activityCreated.addCountries(auxiliar);
+
+    return res.status(200).send("Activity created");
+  } catch (e) {
+    console.error(e);
+    return res.status(404).json({ message: "Creation Failed" });
+  }
+};
+
+const getActivity = async (req, res) => {
+  try {
+    const resposeActivity = await Activity.findAll();
+    res.status(200).send(resposeActivity)
+} catch (e) {
+  return res.status(404).send(e);
+}
+};
+
+module.exports = { postActivity, getActivity };
---
+++ PI-Countries-Henry/api/src/controllers/country.js
@@ -0,0 +1,68 @@
+const { Country, Activity } = require("../db.js");
+const { getFromDb, getFromApi } = require("../utils");
+
+const { Op } = require("sequelize");
+
+const getAllCountries = async (req, res) => {
+  const { name } = req.query;
+
+  try {
+    const countriesDb = await getFromDb();
+
+    if (!name) {
+      return res.send(countriesDb);
+    } else {
+      const nameGood =
+        name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+
+      const nameMatch = await Country.findAll({
+        where: {
+          name: {
+            [Op.like]: `%${nameGood}%`, //que en el medio contenga eso SQL
+          },
+        },
+        include: {
+          model: Activity,
+          attributes: ['name', 'difficulty', 'duration', 'season', 'createdInDb'],
+          through: { attributes: [] },
+        },
+      });
+      nameMatch.length === 0
+        ? res.status(404).send( "We're sorry, no matches were found for your search")
+        : res.status(200).send(nameMatch);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getCountryById = async (req, res) => {
+  
+  const countryId = req.params.idPais.toUpperCase()
+try {
+    let countryFoundId = await Country.findOne( {
+      where: {
+        alpha3Code: countryId,
+      },
+      include: { model: Activity, through: { attributes: [] } },
+      //include: Activity, required: false
+    });
+    if(countryFoundId){
+      res.status(200).send(countryFoundId)
+    }else res.status(404).send("There is no country with that id")
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+
+
+
+
+
+
+
+module.exports = {
+  getAllCountries,
+  getCountryById,
+};
